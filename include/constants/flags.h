9bf454d4e5a1de50f17c2493d08f86c484f19708
BenCappa <ben@thecappas.com>
2025-05-14 18:08:30 -0700

    Opened Desert Underpass and Safari Zone
    Safari Zone is now completely open from the start and Desert Underpass opens once the Mirage Tower falls.
    
    This is instead of having to wait until the post-game.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -763,7 +763,7 @@
 #define FLAG_HIDE_BATTLE_TOWER_MULTI_BATTLE_PARTNER_4               0x2CA
 #define FLAG_HIDE_BATTLE_TOWER_MULTI_BATTLE_PARTNER_5               0x2CB
 #define FLAG_HIDE_BATTLE_TOWER_MULTI_BATTLE_PARTNER_6               0x2CC
-#define FLAG_HIDE_SAFARI_ZONE_SOUTH_CONSTRUCTION_WORKERS            0x2CD
+#define FLAG_HIDE_SAFARI_ZONE_SOUTH_CONSTRUCTION_WORKERS            0x2CD // Referenced in data/scripts/hall_of_fame.inc but is no longer used
 #define FLAG_HIDE_MEW                                               0x2CE
 #define FLAG_HIDE_ROUTE_104_RIVAL                                   0x2CF
 #define FLAG_HIDE_ROUTE_101_BIRCH_ZIGZAGOON_BATTLE                  0x2D0
